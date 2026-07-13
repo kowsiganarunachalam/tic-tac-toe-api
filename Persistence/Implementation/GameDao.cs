a49284c0e3639d249fc92978d1b2e6b0668b89a3
kowsiganarunachalam <kowsiganarunachalam@gmail.com>
2026-07-13 11:46:31 +0530

    Update GameDao.cs - for async and bux fixes

--- Persistence/Implementation/GameDao.cs
+++ Persistence/Implementation/GameDao.cs
@@ -1,7 +1,6 @@
 using tic_tac_toe_api.Persistence.Interface;
-using System.Data;
-using Npgsql;
 using Dapper;
+using Npgsql;
 
 namespace tic_tac_toe_api.Persistence.Implementation;
 
@@ -13,63 +12,73 @@ public class GameDao(string connectionString) : IGameDao
     {
         return new NpgsqlConnection(_connectionString);
     }
-    public async Task<long> InsertRoom(long player,string roomId)
+
+    public async Task<long> InsertRoom(long player, string roomId)
     {
-        var sql = @"
-        INSERT INTO rooms (room_code,created_by) 
-        VALUES (@RoomCode,@CreatedBy) 
-        RETURNING id;";
+        const string sql = @"
+            INSERT INTO rooms (room_code, created_by)
+            VALUES (@RoomCode, @CreatedBy)
+            RETURNING id;";
 
-        using var conn = GetConnection();
-        conn.Open();
+        await using var conn = GetConnection();
+        await conn.OpenAsync();
 
-        //await conn.ExecuteAsync(sql, new { RoomId = roomId, PlayerId = player });
         return await conn.ExecuteScalarAsync<long>(sql, new
         {
             RoomCode = roomId,
             CreatedBy = player
         });
     }
-    public async Task<long> InsertPlayer(string guid,string name)
+
+    public async Task<long> InsertPlayer(string guid, string name)
     {
         const string sql = @"
-            INSERT INTO players (name,playername,created_on) 
-            VALUES (@Name,@PlayerName, now()) 
+            INSERT INTO players (name, playername, created_on)
+            VALUES (@Name, @PlayerName, NOW())
             RETURNING id;";
 
-        using var conn = GetConnection();
-        conn.Open();
-        return await conn.ExecuteScalarAsync<long>(sql, new { Name = guid,PlayerName=name });
+        await using var conn = GetConnection();
+        await conn.OpenAsync();
+
+        return await conn.ExecuteScalarAsync<long>(sql, new
+        {
+            Name = guid,
+            PlayerName = name
+        });
     }
 
     public async Task<long> InsertMatchDetails(long roomId, long winner)
     {
         const string sql = @"
             INSERT INTO matches (room_id, winner, created_on, created_by)
-            VALUES (@RoomId, @Winner, now(), @CreatedBy)
+            VALUES (@RoomId, @Winner, NOW(), @CreatedBy)
             RETURNING id;";
 
-        using var conn = GetConnection();
-        conn.Open();
+        await using var conn = GetConnection();
+        await conn.OpenAsync();
 
-        var id = await conn.ExecuteScalarAsync<long>(sql, new
+        return await conn.ExecuteScalarAsync<long>(sql, new
         {
             RoomId = roomId,
             Winner = winner,
             CreatedBy = winner
         });
-        conn.Close();
-        return id;
     }
 
-    public async Task<long> InsertIntoRoom(long player, long roomId)
+    public async Task<int> InsertIntoRoom(long player, long roomId)
     {
-        var sql = @"
-        INSERT INTO room_players(room_id, player_id) VALUES (@RoomId, @PlayerId)
-                    ON CONFLICT DO NOTHING;";
+        const string sql = @"
+            INSERT INTO room_players (room_id, player_id)
+            VALUES (@RoomId, @PlayerId)
+            ON CONFLICT DO NOTHING;";
 
-        using var conn = GetConnection();
-        conn.Open();
-        return await conn.ExecuteScalarAsync<long>(sql,  new { RoomId = roomId, PlayerId = player });
+        await using var conn = GetConnection();
+        await conn.OpenAsync();
+
+        return await conn.ExecuteAsync(sql, new
+        {
+            RoomId = roomId,
+            PlayerId = player
+        });
     }
 }
